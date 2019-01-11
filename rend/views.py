4f62a4e32c79e22217946882514fa28c308cccf7
beattykariuki <bettykariuki026@gmail.com>
2019-01-11 14:37:24 +0300

    added a text

--- rend/views.py
+++ rend/views.py
@@ -5,7 +5,7 @@ from django.conf.urls import url,include
 from django.contrib.auth import authenticate,login,logout
 from .forms import PostForm,CommentForm
 from django.conf.urls.static import static
-from .models import Profile,Image,Likes
+from .models import Profile,Project,Likes
 from django.contrib.auth.models import User
 from annoying.decorators import ajax_request
 
@@ -19,7 +19,7 @@ def welcome(request):
 
 @login_required(login_url='/accounts/login')
 def index(request):
-    all_images = Image.objects.all()
+    all_projects = Project.objects.all()
     all_users = Profile.objects.all()
     likes = Likes.objects.all()
     next = request.GET.get('next')
@@ -36,7 +36,30 @@ def index(request):
     else:
         form = CommentForm()
     if next:return redirect(next)
-    return render(request, 'showcase/home.html', {"all_images": all_images,"all_users":all_users,'form':form})
+    return render(request, 'showcase/home.html', {"all_projects": all_projects,"all_users":all_users,'form':form})
+
+def project(request,project_id):
+    project = Project.objects.get(id = project_id)
+    rating = round(((project.userinterface + project.functionality )/2),2)
+    if request.method == 'POST':
+        form = VoteForm(request.POST)
+        if form.is_valid:
+            if project.userinterface == 1:
+                project.userinterface = int(request.POST['userinterface'])
+            else:
+                project.userinterface = (project.userinterface + int(request.POST['userinterface']))/2
+            if project.functionality == 1:
+                project.functionality = int(request.POST['functionality'])
+            else:
+                project.functionality = (project.userinterface + int(request.POST['functionality']))/2
+            if project.content == 1:
+                project.content = int(request.POST['content'])
+            else:
+                project.content = (project.design + int(request.POST['content']))/2
+    else:
+        form = VoteForm()
+    return render(request,'project.html',{'form':form,'project':project,'rating':rating})
+
 
 @login_required(login_url='/accounts/login/')
 def explore(request):
@@ -56,12 +79,12 @@ def login(request):
 def upload(request):
     current_user = request.user
     p = Profile.objects.filter(id=current_user.id).first()
-    imageuploader_profile = Image.objects.filter(imageuploader_profile=p).all()
+    # uploader_profile = Project.objects.filter(projectuploader_profile=p).all()
     if request.method == 'POST':
         form = PostForm(request.POST,request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            post.imageuploader_profile= p
+            post.projectuploader_profile= p
             post.save()
             return redirect('/')
     else:
@@ -70,20 +93,40 @@ def upload(request):
 
 @login_required(login_url='/accounts/login/')
 def like(request, image_id):
-    image = get_object_or_404(Image, pk=image_id)
-    request.user.profile.like(image)
-    return JsonReponse(image.count_likes, safe=False)
+    project = get_object_or_404(Project, pk=image_id)
+    request.user.profile.like(project)
+    return JsonReponse(project.count_likes, safe=False)
 
 @login_required(login_url='/accounts/login/')
-def unlike(request, image_id):
+def unlike(request, project_id):
     image = get_object_or_404(Image, pk=post_id)
-    request.user.profile.unlike(image)
+    request.user.profile.unlike(project)
     return JsonReponse(image.count_likes, safe=False)
 
     
 
 def profile(request):
     return render(request,'showcase/profile.html')
+
+def vote_project(request, project_id):
+    project = Project.objects.get(id=project_id)
+    rating = round(((project.userinterface + project.functionality)/2),2)
+    if request.method == 'POST':
+        form = VoteForm(request.POST)
+        if form.is_valid:
+            if project.userinterface == 1:
+                project.userinterface = int(request.POST['userinterface'])
+            else:
+                project.userinterface = (project.userinterface + int(request.POST['userinterface']))/2
+            if project.functionality == 1:
+                project.functionality = int(request.POST['functionality'])
+            else:
+                project.functionality = (project.userinterface + int(request.POST['functionality']))/2
+            if project.content == 1:
+                project.content = int(request.POST['content'])
+            else:
+                project.content = (project.design + int(request.POST['content']))/2
+
 
 def add_comment_to_post(request):
     post = get_object_or_404(Post, pk=pk)
