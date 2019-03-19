0cd18da2a694c6c98c017b5c0f722a1cac3849fe
beattykariuki <bettykariuki026@gmail.com>
2019-03-19 10:07:06 +0300

    deployment

--- itt/settings.py
+++ itt/settings.py
@@ -144,7 +144,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-
+# https://docs.djangoproject.com/en/1.11/howto/static-files/STATIC_URL = '/static/'
+ Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
@@ -152,10 +153,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
