cce0b3ad7801ecc3e05d1787fbd6a6addaa11bdf
beattykariuki <bettykariuki026@gmail.com>
2019-03-19 10:09:19 +0300

    deployment

--- itt/settings.py
+++ itt/settings.py
@@ -145,7 +145,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/STATIC_URL = '/static/'
- Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
